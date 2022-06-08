6e08d310f3446b17e40d2572ae4100789b9200af
Monijo <mokajozefowicz@gmail.com>
2022-06-08 19:41:18 +0200

    add views and use templates

--- app_with_polls/polls/views.py
+++ app_with_polls/polls/views.py
@@ -1,5 +1,27 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello hello")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = 'polls/index.html'
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, template, context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/details.html", {"question":question})
+
+
+def results(request, question_id):
+    response = f"You are looking for results of question {question_id}"
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting for question {question_id}")
