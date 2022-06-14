e4f914e35a3dc2bddcb4b30f6f40a3585d9eea09
Monijo <mokajozefowicz@gmail.com>
2022-06-14 17:21:46 +0200

    customize admin site

--- app_with_polls/app_with_polls/settings.py
+++ app_with_polls/app_with_polls/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'app_with_polls.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
